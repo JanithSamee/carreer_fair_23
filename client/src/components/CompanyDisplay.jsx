d55d3b55f79fffaccfd3881ddedc0dc7a5b0dd07
geshan99 <geshansudasinghe@gmail.com>
2023-04-14 21:11:14 +0530

    company details modal bug fixed

--- client/src/components/CompanyDisplay.jsx
+++ client/src/components/CompanyDisplay.jsx
@@ -28,12 +28,12 @@ function CompanyDisplay({ name, img_url }) {
 			<CompanyDisplayModal
 				isOpen={isOpen}
 				onClose={onClose}
-				title={"Edit Company"}
+				title={name}
 				companyName={name}
 				companyDes={
 					"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
 				}
-				email={"carreres@wso2.com"}
+				email={"careers@wso2.com"}
 				vacancies={5}
 			></CompanyDisplayModal>
 		</div>
