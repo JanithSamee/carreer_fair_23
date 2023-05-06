0f20265618c4e66ca27f5b7f7c216cfff6a40d4c
geshan99 <geshansudasinghe@gmail.com>
2023-05-06 23:28:12 +0530

    add pref component warning fixed

--- client/src/components/AddPrefComp.jsx
+++ client/src/components/AddPrefComp.jsx
@@ -1,15 +1,15 @@
 import {
-    Grid,
-    GridItem,
-    Text,
-    Box,
-    FormControl,
-    FormLabel,
-    Select,
-    Button,
-    Spacer,
-    Flex,
-    useToast,
+	Grid,
+	GridItem,
+	Text,
+	Box,
+	FormControl,
+	FormLabel,
+	Select,
+	Button,
+	Spacer,
+	Flex,
+	useToast,
 } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { useState, useEffect } from "react";
@@ -20,237 +20,241 @@ import { getCompanyName } from "../utils/helpers/formatter";
 import { assigneInterview } from "../utils/api/interview.api";
 
 function AddPrefComp({ index_number }) {
-    const toast = useToast();
-    //states
-    const [company, setCompany] = useState("undefined");
-    const [loading, setloading] = useState(false);
-    const [time, setTime] = useState();
-    const [error, setError] = useState();
-    const [interveiwList, setInterveiwList] = useState([]);
-    const [companyList, setCompanyList] = useState([{}]);
-    //APIs
+	const toast = useToast();
+	//states
+	const [company, setCompany] = useState("undefined");
+	const [loading, setloading] = useState(false);
+	const [time, setTime] = useState();
+	const [error, setError] = useState();
+	const [interveiwList, setInterveiwList] = useState([]);
+	const [companyList, setCompanyList] = useState([{}]);
+	//APIs
 
-    async function handleSave() {
-        const checkSameTime = interveiwList.filter((obj, index, arr) =>
-            arr.slice(index + 1).some((otherObj) => otherObj.time === obj.time)
-        );
+	async function handleSave() {
+		const checkSameTime = interveiwList.filter((obj, index, arr) =>
+			arr.slice(index + 1).some((otherObj) => otherObj.time === obj.time)
+		);
 
-        if (checkSameTime.length > 0) {
-            return toast({
-                title: "An error occurred.",
-                description:
-                    "Interviews must be separated from time slots. Duplicate time slots found!",
-                status: "error",
-                duration: 9000,
-                isClosable: true,
-            });
-        }
-        setloading(true);
+		if (checkSameTime.length > 0) {
+			return toast({
+				title: "An error occurred.",
+				description:
+					"Interviews must be separated from time slots. Duplicate time slots found!",
+				status: "error",
+				duration: 9000,
+				isClosable: true,
+			});
+		}
+		setloading(true);
 
-        const _res = await assigneInterview({
-            indexNumber: index_number,
-            interviews: interveiwList,
-        });
-        setloading(false);
+		const _res = await assigneInterview({
+			indexNumber: index_number,
+			interviews: interveiwList,
+		});
+		setloading(false);
 
-        if (_res.error) {
-            toast({
-                title: "An error occurred.",
-                description: _res.data,
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-        } else {
-            toast({
-                title: "Done",
-                description: "Interview Assigened Successfully!",
-                status: "success",
-                duration: 2000,
-                isClosable: true,
-            });
-            // setAddedCount(addedCount + 1);
-        }
-    }
+		if (_res.error) {
+			toast({
+				title: "An error occurred.",
+				description: _res.data,
+				status: "error",
+				duration: 5000,
+				isClosable: true,
+			});
+		} else {
+			toast({
+				title: "Done",
+				description: "Interview Assigened Successfully!",
+				status: "success",
+				duration: 2000,
+				isClosable: true,
+			});
+			// setAddedCount(addedCount + 1);
+		}
+	}
 
-    useEffect(() => {
-        async function getData() {
-            // setloadingCompanies(true);
-            setloading(true);
-            const _res = await getAllCompanies();
-            setloading(false);
-            // setloadingCompanies(false);
-            if (_res.error) {
-                return toast({
-                    title: "An error occurred.",
-                    description: _res.data,
-                    status: "error",
-                    duration: 9000,
-                    isClosable: true,
-                });
-            }
-            Array.isArray(_res.data) && setCompanyList(_res.data);
-        }
+	useEffect(() => {
+		async function getData() {
+			// setloadingCompanies(true);
+			setloading(true);
+			const _res = await getAllCompanies();
+			setloading(false);
+			// setloadingCompanies(false);
+			if (_res.error) {
+				return toast({
+					title: "An error occurred.",
+					description: _res.data,
+					status: "error",
+					duration: 9000,
+					isClosable: true,
+				});
+			}
+			Array.isArray(_res.data) && setCompanyList(_res.data);
+		}
 
-        getData();
-    }, []);
+		getData();
+	}, []);
 
-    // useEffect(() => {
-    //     interviewsList && setInterveiwList(interviewsList);
-    // }, [interviewsList]);
+	// useEffect(() => {
+	//     interviewsList && setInterveiwList(interviewsList);
+	// }, [interviewsList]);
 
-    //event handling
-    const handleChangeCompany = (e) => setCompany(e.target.value);
-    const handleChangeTime = (e) => setTime(e.target.value);
-    function handleAssign() {
-        if (
-            company === undefined ||
-            time === undefined ||
-            company === "" ||
-            time === ""
-        ) {
-            setError("Required !");
-        } else {
-            const isAdded = interveiwList.find((u) => u.company === company);
-            if (!isAdded) {
-                const newList = interveiwList.concat({
-                    company,
-                    time,
-                    name: company,
-                });
-                setInterveiwList(newList);
-            }
-        }
-    }
-    const assignedInterveiws = () =>
-        interveiwList.map((interveiw, idx) => (
-            <Flex
-                boxShadow="sm"
-                px={1}
-                hidden={interveiw.company === undefined}
-                justify="center"
-                align="center"
-                key={idx}
-            >
-                <Box>
-                    <Text fontSize="xs">
-                        {interveiw.company && getCompanyName(interveiw.company)}
-                    </Text>
-                </Box>
-                <Spacer></Spacer>
-                <CloseButton
-                    interveiw={interveiw}
-                    interveiwList={interveiwList}
-                    setInterveiwList={setInterveiwList}
-                ></CloseButton>
-            </Flex>
-        ));
-    const companies = () =>
-        companyList.map((com, idx) => (
-            <option key={idx} value={com.companyId}>
-                {com.companyId && getCompanyName(com.companyId)}
-            </option>
-        ));
+	//event handling
+	const handleChangeCompany = (e) => setCompany(e.target.value);
+	const handleChangeTime = (e) => setTime(e.target.value);
+	function handleAssign() {
+		if (
+			company === undefined ||
+			time === undefined ||
+			company === "" ||
+			time === ""
+		) {
+			setError("Required !");
+		} else {
+			const isAdded = interveiwList.find((u) => u.company === company);
+			if (!isAdded) {
+				const newList = interveiwList.concat({
+					company,
+					time,
+					name: company,
+				});
+				setInterveiwList(newList);
+			}
+		}
+	}
+	const assignedInterveiws = () =>
+		interveiwList.map((interveiw, idx) => (
+			<Flex
+				boxShadow="sm"
+				px={1}
+				hidden={interveiw.company === undefined}
+				justify="center"
+				align="center"
+				key={idx}
+			>
+				<Box>
+					<Text fontSize="xs">
+						{interveiw.company && getCompanyName(interveiw.company)}
+					</Text>
+				</Box>
+				<Spacer></Spacer>
+				<CloseButton
+					interveiw={interveiw}
+					interveiwList={interveiwList}
+					setInterveiwList={setInterveiwList}
+				></CloseButton>
+			</Flex>
+		));
+	const companies = () =>
+		companyList.map((com, indx) => (
+			<option value={com.companyId} key={indx}>
+				{com.companyId && getCompanyName(com.companyId)}
+			</option>
+		));
 
-    const OptionsList = () => {
-        const _lst = [];
-        for (let hour = 8; hour <= 22; hour++) {
-            // Loop to generate 4 quarter-hour options for each hour
-            for (let minute = 0; minute <= 45; minute += 15) {
-                // Format the hour and minute values as "hh.mm"
-                let time = `${hour.toString().padStart(2, "0")}.${minute
-                    .toString()
-                    .padStart(2, "0")}`;
-                // Create an option element with the formatted time value
+	const OptionsList = () => {
+		const _lst = [];
+		for (let hour = 8; hour <= 22; hour++) {
+			// Loop to generate 4 quarter-hour options for each hour
+			for (let minute = 0; minute <= 45; minute += 15) {
+				// Format the hour and minute values as "hh.mm"
+				let time = `${hour.toString().padStart(2, "0")}.${minute
+					.toString()
+					.padStart(2, "0")}`;
+				// Create an option element with the formatted time value
 
-                _lst.push(<option value={time}>{time}</option>);
-            }
-        }
-        return _lst;
-    };
+				_lst.push(
+					<option value={time} key={time}>
+						{time}
+					</option>
+				);
+			}
+		}
+		return _lst;
+	};
 
-    return (
-        <div>
-            <Grid
-                templateColumns="repeat(2, 2fr)"
-                gap={8}
-                opacity={loading ? "0.8" : "1"}
-            >
-                <GridItem justify="center">
-                    <Box boxShadow="md" p={5} mt={2}>
-                        <FormControl>
-                            <FormLabel fontSize="xs">Company</FormLabel>
-                            <Select
-                                size="xs"
-                                placeholder="Select Company"
-                                mb={3}
-                                required
-                                onChange={handleChangeCompany}
-                            >
-                                {companies()}
-                            </Select>
-                            <FormLabel fontSize="xs">Time Slot</FormLabel>
-                            <Select
-                                size="xs"
-                                placeholder=" Time Slot"
-                                required
-                                onChange={handleChangeTime}
-                            >
-                                {OptionsList()}
-                            </Select>
-                        </FormControl>
-                        <Button
-                            rightIcon={<ArrowForwardIcon />}
-                            mt={3}
-                            size="xs"
-                            bg="teal.300"
-                            onClick={handleAssign}
-                            isDisabled={
-                                time === undefined || company === undefined
-                            }
-                        >
-                            Assign Interveiw
-                        </Button>
-                    </Box>
-                </GridItem>
-                <GridItem justify="center">
-                    <Box boxShadow="md" p={5} mt={2} minH={190}>
-                        {interveiwList.map((interveiw, idx) => (
-                            <Flex
-                                boxShadow="sm"
-                                px={1}
-                                hidden={interveiw.company === undefined}
-                                justify="center"
-                                align="center"
-                                key={idx}
-                            >
-                                <Box>
-                                    <Text fontSize="xs">
-                                        {interveiw.company &&
-                                            getCompanyName(interveiw.company)}
-                                    </Text>
-                                </Box>
-                                <Spacer></Spacer>
-                                <CloseButton
-                                    interveiw={interveiw}
-                                    interveiwList={interveiwList}
-                                    setInterveiwList={setInterveiwList}
-                                ></CloseButton>
-                            </Flex>
-                        ))}
-                    </Box>
-                </GridItem>
-            </Grid>
-            <Button
-                mt={2}
-                onClick={handleSave}
-                isLoading={loading}
-                loadingText={"Loading..."}
-            >
-                save
-            </Button>
-        </div>
-    );
+	return (
+		<div>
+			<Grid
+				templateColumns="repeat(2, 2fr)"
+				gap={8}
+				opacity={loading ? "0.8" : "1"}
+			>
+				<GridItem justify="center">
+					<Box boxShadow="md" p={5} mt={2}>
+						<FormControl>
+							<FormLabel fontSize="xs">Company</FormLabel>
+							<Select
+								size="xs"
+								placeholder="Select Company"
+								mb={3}
+								required
+								onChange={handleChangeCompany}
+							>
+								{companies()}
+							</Select>
+							<FormLabel fontSize="xs">Time Slot</FormLabel>
+							<Select
+								size="xs"
+								placeholder=" Time Slot"
+								required
+								onChange={handleChangeTime}
+							>
+								{OptionsList()}
+							</Select>
+						</FormControl>
+						<Button
+							rightIcon={<ArrowForwardIcon />}
+							mt={3}
+							size="xs"
+							bg="teal.300"
+							onClick={handleAssign}
+							isDisabled={
+								time === undefined || company === undefined
+							}
+						>
+							Assign Interveiw
+						</Button>
+					</Box>
+				</GridItem>
+				<GridItem justify="center">
+					<Box boxShadow="md" p={5} mt={2} minH={190}>
+						{interveiwList.map((interveiw, idx) => (
+							<Flex
+								boxShadow="sm"
+								px={1}
+								hidden={interveiw.company === undefined}
+								justify="center"
+								align="center"
+								key={idx}
+							>
+								<Box>
+									<Text fontSize="xs">
+										{interveiw.company &&
+											getCompanyName(interveiw.company)}
+									</Text>
+								</Box>
+								<Spacer></Spacer>
+								<CloseButton
+									interveiw={interveiw}
+									interveiwList={interveiwList}
+									setInterveiwList={setInterveiwList}
+								></CloseButton>
+							</Flex>
+						))}
+					</Box>
+				</GridItem>
+			</Grid>
+			<Button
+				mt={2}
+				onClick={handleSave}
+				isLoading={loading}
+				loadingText={"Loading..."}
+			>
+				save
+			</Button>
+		</div>
+	);
 }
 
 export default AddPrefComp;
--- client/src/components/StudentModalContent.jsx
+++ client/src/components/StudentModalContent.jsx
@@ -5,58 +5,58 @@ import { useEffect, useState } from "react";
 import { getStudent } from "../utils/api/student.api";
 
 function StudentModalContent({
-    name,
-    img_url,
-    name_initial,
-    index_number,
-    interviewsList,
+	name,
+	img_url,
+	name_initial,
+	index_number,
+	interviewsList,
 }) {
-    const toast = useToast();
-    const [isLoading, setisLoading] = useState(false);
-    const [studentData, setstudentData] = useState({});
+	const toast = useToast();
+	const [isLoading, setisLoading] = useState(false);
+	const [studentData, setstudentData] = useState({});
 
-    useEffect(() => {
-        async function getData() {
-            setisLoading(true);
-            const _res = await getStudent(index_number);
-            setisLoading(false);
-            // console.log(_res);
-            if (_res && !_res.error) {
-                setstudentData(_res.data);
-            } else {
-                toast({
-                    title: "An error occurred.",
-                    description: _res.data,
-                    status: "error",
-                    duration: 5000,
-                    isClosable: true,
-                });
-            }
-        }
-        getData();
-    }, []);
-    return (
-        <div>
-            <Stack align="center" spacing={0.5}>
-                <Avatar name={name} size="lg" src={img_url}></Avatar>
-                <Text>{index_number}</Text>
-                <Text>{name_initial}</Text>
-                <CompanyList
-                    type="pref"
-                    isLoading={isLoading}
-                    companyList={studentData.preferenceList}
-                ></CompanyList>
-                <CompanyList
-                    isLoading={isLoading}
-                    companyList={studentData.interviewsList}
-                ></CompanyList>
-                <AddPrefComp
-                    index_number={index_number}
-                    interviewsList={studentData.interviewsList}
-                ></AddPrefComp>
-            </Stack>
-        </div>
-    );
+	useEffect(() => {
+		async function getData() {
+			setisLoading(true);
+			const _res = await getStudent(index_number);
+			setisLoading(false);
+			// console.log(_res);
+			if (_res && !_res.error) {
+				setstudentData(_res.data);
+			} else {
+				toast({
+					title: "An error occurred.",
+					description: _res.data,
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+				});
+			}
+		}
+		getData();
+	}, []);
+	return (
+		<div>
+			<Stack align="center" spacing={0.5}>
+				<Avatar name={name} size="lg" src={img_url}></Avatar>
+				<Text>{index_number}</Text>
+				<Text>{name_initial}</Text>
+				<CompanyList
+					type="pref"
+					isLoading={isLoading}
+					companyList={studentData.preferenceList}
+				></CompanyList>
+				<CompanyList
+					isLoading={isLoading}
+					companyList={studentData.interviewsList}
+				></CompanyList>
+				<AddPrefComp
+					index_number={index_number}
+					interviewsList={studentData.interviewsList}
+				></AddPrefComp>
+			</Stack>
+		</div>
+	);
 }
 
 export default StudentModalContent;
